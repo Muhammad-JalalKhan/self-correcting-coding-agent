550d93b76320feab139518f11c5ab390ed535ac6
Muhammad Jalal <itxjalalkhanofficial@gmail.com>
2026-02-03 22:48:16 +0500

    mathe test code is here

--- testcases/math_test.py
+++ testcases/math_test.py
@@ -1,4 +1,4 @@
-# Fixed version using sum wit generator expression
+# Fixed version using sum wit generator expressions 
 numbers = ['5', '10', '15']
 total = sum(int(n) for n in numbers)
 print(f"Total: {total}")
