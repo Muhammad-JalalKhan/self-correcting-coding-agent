b205ff02709ff6b844b2c3d9bf21e167e7144d46
Jalal Khan <itxjalalkhanofficial@gmail.com>
2026-03-19 23:45:50 +0500

    commit in frontend file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* Remove the experimental block for now */
 };
 
 export default nextConfig;
